45d3d05b656e2ce819fbdf32c0fbb3ac4902ec0a
Aditya raj <meadityaraj0001@gmail.com>
2023-12-06 08:43:25 +0530

    add loader

--- script.js
+++ script.js
@@ -1,15 +1,13 @@
 gsap.to("#page1 h1",{
-    delay:1,
     transform:"translateX(-100%)",
     fontWeight:"100",
-    duration:1,
     scrollTrigger:{
         trigger:"#page1",
         scroller:"body",
         // markers:true,
         start:"top 0",
-        end:"top -100%",
-        scrub:2,
+        end:"top -200%",
+        scrub:3,
         pin:true,
          
 
